3a80a7f6e892d5c5a8e1d105ffee6d343d0a0559
Joowon Hong <ghdwn6687@gmail.com>
2020-12-26 14:58:10 +0900

    로그인 만들기

--- fc_community/fcuser/views.py
+++ fc_community/fcuser/views.py
@@ -1,8 +1,45 @@
-from django.shortcuts import render
+from django.shortcuts import render,redirect
 from django.http import HttpResponse
 from .models import Fcuser
-from django.contrib.auth.hashers import make_password
+from django.contrib.auth.hashers import make_password,check_password
 # Create your views here.
+
+def home(request):
+    user_id  = request.session.get('user')
+
+    if user_id:
+        fcuser = Fcuser.objects.get(pk=user_id)
+        return HttpResponse(fcuser.username)
+    return HttpResponse('Home!')
+
+
+
+def login(request):
+    if request.method=="GET":
+        return render(request,'login.html')
+
+    elif request.method=="POST":
+        username=request.POST.get('username',None)
+        password=request.POST.get('password',None) 
+
+        res_data={}
+        if not (username and password):
+            res_data['error']= '모든 항목을 입력하세요.'
+        else:
+            fcuser = Fcuser.objects.get(username=username)
+            if check_password(password,fcuser.password):
+                request.session['user']=fcuser.id
+
+                return redirect('/')
+
+                #비밀번호가 일치,로그인 처리를!
+               
+            else:
+                res_data['error']='비밀번호가 틀렸습니다.'
+            
+        return render(request,'login.html',res_data)
+
+
 
 def register(request):
     if request.method =="GET":
@@ -17,7 +54,7 @@ def register(request):
 
         if not (username and password and re_password and e_mail):
              res_data['error'] ='모든 항목을 입력해야 합니다!'
-        if password!=re_password:
+        elif password!=re_password:
             res_data['error'] ='비밀번호가 다릅니다!'
         else:
             fcuser= Fcuser(
