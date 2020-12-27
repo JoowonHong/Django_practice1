be3c10f8ad07936df9c31a0f681bb36b0d5f2ed0
Joowon Hong <ghdwn6687@gmail.com>
2020-12-27 15:00:59 +0900

    Login form 만들기

--- fc_community/fcuser/views.py
+++ fc_community/fcuser/views.py
@@ -2,6 +2,7 @@ from django.shortcuts import render,redirect
 from django.http import HttpResponse
 from .models import Fcuser
 from django.contrib.auth.hashers import make_password,check_password
+from .forms import LoginForm
 # Create your views here.
 
 def home(request):
@@ -18,29 +19,8 @@ def logout(request):
     return redirect('/')
 
 def login(request):
-    if request.method=="GET":
-        return render(request,'login.html')
-
-    elif request.method=="POST":
-        username=request.POST.get('username',None)
-        password=request.POST.get('password',None) 
-
-        res_data={}
-        if not (username and password):
-            res_data['error']= '모든 항목을 입력하세요.'
-        else:
-            fcuser = Fcuser.objects.get(username=username)
-            if check_password(password,fcuser.password):
-                request.session['user']=fcuser.id
-                
-                return redirect('/')
-
-                #비밀번호가 일치,로그인 처리를!
-               
-            else:
-                res_data['error']='비밀번호가 틀렸습니다.'
-            
-        return render(request,'login.html',res_data)
+    form= LoginForm()
+    return render(request,'login.html',{'form':form})
 
 
 
